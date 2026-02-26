88a3297a11a09af1440b5b805e18979516ea0e00
amaan2t3 <amankacho0001@gmail.com>
2026-02-26 16:15:49 +0500

    headers and http status custom middleware or plugin

--- Server/restApi.js
+++ Server/restApi.js
@@ -8,7 +8,20 @@ const PORT = 8000;
 
 const app = express()
 /////////////////Middleware --- Plugin
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // VERY IMPORTANT for Postman JSON
+
+//////////Custom Middleware --------------- Plugin
+app.use((req, res, next) => {
+    fs.appendFile("log.txt",
+        `${Date.now()}: ${req.ip} ${req.method}: ${req.path}\n`,
+        (err, data) => {
+            next();
+        }
+    );
+});
+
+
 /////////home page
 app.get("/", (req, res) => {
     res.send("Server is running 🚀");
@@ -19,20 +32,24 @@ app.get("/users", (req, res) => {
     const html = `
     <ul>
        ${users.map((user) => `<li>${user.first_name} </li>`).join("")}
-    </ul>
-    `
+    </ul> `
     res.send(html)
 })
 //////////////////////// JSON Render
 
 app.get("/api/users", (req, res) => {
-    return res.json(users)
+    res.setHeader("X-MyName", "Amaan");
+    console.log(req.headers); ///////////// custom header always add X to custom header
+
+    return res.status(201).json(users);
 })
 
 //////////////////get only one user
 app.route("/api/users/:id") ///////////////  only one user
     .get((req, res) => {
+
         const id = Number(req.params.id);
+        if (!id)  return res.status(404).json("Not Found")
 
         const user = users.find((user) => user.id === id);
         if (!user) {
@@ -41,11 +58,11 @@ app.route("/api/users/:id") ///////////////  only one user
 
         return res.json(user);
     })  ////////////// edit user
-    .patch((req, res) =>{
+    .patch((req, res) => {
         const id = Number(req.params.id)
-        const userIndex = users.findIndex((user)=> user.id === id)
+        const userIndex = users.findIndex((user) => user.id === id)
     })
-      /// delete
+    /// delete
     .delete((req, res) => {
         const id = Number(req.params.id);
 
@@ -65,17 +82,23 @@ app.post("/api/users", (req, res) => {
     ///// user create
     const body = req.body;
     console.log("body:", body)
+    if (!body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.jobtitle) {
+        return res.status(400).json("Bad Request")
+
+    }
     users.push({ ...body, id: users.length + 1 })
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-
-        return res.json({ status: "Success", id: users.length })
-    })
-
-
-
-
-
-})
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        if (err) {
+            return res.status(500).json({ status: "error" });
+        }
+        return res.json({ status: "Success", id: users.length });
+    });
+});
 
 
 
