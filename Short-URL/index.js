3c1ecb340bdd967ea6dd435849433019968e168f
amaan2t3 <amankacho0001@gmail.com>
2026-03-18 16:21:25 +0500

    short URL UI design with ejs

--- Short-URL/index.js
+++ Short-URL/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const urlRoute = require("./routes/urlRoute");
 const { connectToMongoDB } = require("./connectMD");
 
+
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("Mongodb Connected"),
 );
+
+//////// ejs
+
+ 
+
 
 app.use(express.json());
 
