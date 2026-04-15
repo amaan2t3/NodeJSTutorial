47ce79d97bd36a11dc2dc93f94129f353e540ce1
amaan2t3 <amankacho0001@gmail.com>
2026-04-15 14:17:03 +0500

    feat: update dependencies and switch to ES module syntax
    - Added discord.js and dotenv as dependencies.
    - Updated package-lock.json with new versions and their dependencies.
    - Changed package type from commonjs to module in package.json.

---
+++ DiscordBot/index.js
@@ -0,0 +1,40 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent]
+});
+
+client.on("messageCreate", (message) => {
+    // Ignore messages from bots (including this bot)
+    if (message.author.bot) return;
+    // Ignore messages where the bot is not mentioned or start with a command
+    if (!message.guild) return;
+    
+    if (message.content.startsWith("create")) {
+        const url = message.content.split("create")[1];
+        return message.reply({
+            content: "Generting Short ID For " + url
+        });
+    }
+    
+    return message.reply({
+        content: "hey there you said " + message.content
+    });
+});
+
+client.on("interactionCreate", (interaction) => {
+    console.log(interaction);
+    interaction.reply("Hello World");
+
+})
+
+client.login(process.env.DISCORD_TOKEN)
+
+client.openai = new OpenAI({
+    apiKey: process.env.OPEN_AI_KEY
+})
--- Short-URL/index.js
+++ Short-URL/index.js
@@ -20,20 +20,21 @@ const  userRoute = require("./routes/userRoute")
 
 // Import Path and Cookies
 const path = require("path");
-
 const cookieParser = require("cookie-parser");
-
 app.use(cookieParser());
+ 
+
+
 
 // Import Auth Middleware
 const { restrictToLoginUserOnly, checkUserLogin } = require("./middlewares/auth");
 
-//////// ejs
+/////////////// Set View Engine and Views Folder  
  app.set("view engine", "ejs");
  app.set("views",path.resolve("./views"));
 
 
-/////////////////// alll Routes
+////////////////// Use Routes 
 app.use("/", checkUserLogin ,staticRouter);
 app.use("/url" , restrictToLoginUserOnly, urlRoute);
 app.use("/user" , userRoute);
